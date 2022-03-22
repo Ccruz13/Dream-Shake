fc9e023533a0ed38c9fec83b5f4af4dc32a81f31
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-03-22 13:27:06 -0400

    started home page and navbar

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,10 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Badge } from "@material-ui/core";
+import { ShoppingCartOutlined } from "@material-ui/icons";
 
 const Navbar = () => {
+
+    const Container = styled.div`
+        height: 60px;
+    `
+    const Wrapper = styled.div`
+        padding: 15px 25px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    `
+    const Left = styled.h1`
+        font-weight: Bold;
+    `;
+    const Right = styled.div`
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    `;
+
+    const MenuItem = styled.div`
+        font-size: 14px;
+        cursor: pointer;
+        margin-left: 25px;
+    `;
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    Dream Shake
+                </Left>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={2} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
     )
 }
 
