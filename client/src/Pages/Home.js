2aa36e493c092619cf9514d20106e0c9bbf64210
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-03-22 17:18:19 -0400

    added carousel and data

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Carousel from '../Components/Carousel'
 import Navbar from '../Components/Navbar'
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Carousel />
         </div>
     )
 }
---
+++ client/src/data.js
@@ -0,0 +1,21 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.flightclub.com/1250/TEMPLATE/248952/1.jpg",
+      title: "AIR JUAN 11s",
+      desc: "NEW RELEASE | ALL SIZES",
+    
+    },
+    {
+      id: 2,
+      img: "https://cdn.flightclub.com/2600/TEMPLATE/806196/1.jpg",
+      title: "LIKES OFF-WHITE",
+      desc: "NEW RELEASE | ALL SIZES",
+    },
+    {
+      id: 3,
+      img: "https://cdn.flightclub.com/1250/TEMPLATE/802501/1.jpg",
+      title: "BREEZY BOOST",
+      desc: "NEW RELEASE | ALL SIZES",
+    },
+  ];
