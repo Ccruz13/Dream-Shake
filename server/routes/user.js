499512f89316a298d8e47c1cf26fb2fba708803f
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-04-26 21:32:47 -0400

    deploy heroku

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verify");
 const router = require("express").Router();
 
